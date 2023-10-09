51551627c5bf90dae1bf42545d5af65d61cc2d4f
Bohdan Khomych <bod.khomych@gmail.com>
2023-10-09 23:09:36 +0300

    solve tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+  private signature: string;
+
+  getSignature() {
+    return this.signature;
+  }
+
+  constructor() {
+    this.signature = '№' + Math.random();
+  }
+}
+
+class Person {
+  private key: Key;
+
+  getKey() {
+    return this.key;
+  }
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('We have a new tenant!');
+    } else {
+      console.log('The door is locked!');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key === this.key) {
+      this.door = true;
+      console.log('The door was opened!');
+    } else {
+      console.log('Wrong key!');
+    }
+  }
+
+  constructor(key: Key) {
+    super(key);
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +66,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
